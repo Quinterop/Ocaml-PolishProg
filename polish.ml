c3bff1c25a5317c038403ea272138162f2712f0a
yanoux <yaniakli9@gmail.com>
2021-12-14 20:58:57 +0100

    convertion des fichiers .p en program pour test eval et print

--- polish.ml
+++ polish.ml
@@ -46,6 +46,49 @@ and block = (position * instr) list
 (** Un programme Polish est un bloc d'instructions *)
 type program = block
 
+let abs:program = [
+  (1, Read("n"));
+  (2, If(
+    (Var("n"), Lt, Num(0)),
+    [
+      (1, Set("res", Op(Sub, Num(0), Var("n"))));
+    ],
+    [
+      (1, Set("res", Var("n")));
+    ]
+  ));
+  (3, Print(Var("res")));
+]
+
+
+let factors:program = [
+  (1, Read("n"));
+  (2, If(
+    (Var("n"), Le, Num(0)),
+    [
+      (1, Print(Num(-1)));
+    ], 
+    [ 
+      (1, Set("i", Num(2)));
+      (2, While(
+        (Op(Mul, Var("i"), Var("i")), Le, Var("n")), 
+        [
+          (1, If(
+            (Op(Mod, Var("n"), Var("i")), Eq, Num(0)), 
+            [
+              (1, Print(Var("i")));
+              (2, Set("n", Op(Div, Var("n"), Var("i"))));
+            ],
+            [
+              (1, Set("i", Op(Add, Var("i"), Num(1))));
+            ]
+          ));
+        ]
+      ));
+      (3, Print(Var("n")));
+    ]
+  ));
+]
 
 (***********************************************************************)
 
